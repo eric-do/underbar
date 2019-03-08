30d93803cc8187c27ffd88095908967ff7aff7d6
ericdo <ericdo.617@gmail.com>
2019-03-08 15:57:32 -0800

    Solved _.flatten() with recursion

--- src/underbar.js
+++ src/underbar.js
@@ -480,6 +480,16 @@
   //
   // Hint: Use Array.isArray to check if something is an array
   _.flatten = function(nestedArray, result) {
+    if (!result) result = [];
+
+    for (var i = 0; i < nestedArray.length; i++) {
+      if (Array.isArray(nestedArray[i])){
+        _.flatten(nestedArray[i], result);
+      } else {
+        result.push(nestedArray[i]);
+      }
+    }
+    return result;
   };
 
   // Takes an arbitrary number of arrays and produces an array that contains
